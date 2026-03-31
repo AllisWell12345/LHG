cb10d07a8cad1f7f694e0dcd3cea5ffac4e7ceaa
zana0422-creator <zana0422@g.eulji.ac.kr>
2026-03-31 18:31:36 +0900

    0331 미션입니다.

--- 0331/0331_mission.jsx
+++ 0331/0331_mission.jsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [answer, setAnswer] = useState("");
   const [input, setInput] = useState("");
-  console.log("?", answer);
+  const [startTime, setStartTime] = useState(Date.now());
+  console.log("?!", input);
 
   useEffect(() => {
     setAnswer(prompt("문자열을 입력해주세요"));
@@ -11,19 +12,29 @@ function App() {
     console.log("????", input);
   }, []);
   const onChangeHandler = (e) => {
-    const startTime = new Date().getTime();
+    // setStartTime(Date.now());
     setInput(e.target.value);
-    console.log(startTime);
-    if (answer === input) {
-      const endTime = new Date().getTime();
-      console.log(endTime);
-      alert(`${Number(endTime) - Number(startTime)}ms`);
+    console.log("start", startTime);
+    console.log("!", input);
+  };
+  const onKeyDownHandler = (e) => {
+    if (e.key === "Enter") {
+      if (answer === input) {
+        const endTime = Date.now();
+        console.log("end", endTime);
+        alert(`${Number(endTime) - Number(startTime)}ms`);
+      }
     }
   };
   return (
     <>
       <h1>{answer}</h1>
-      <input type="text" value={input} onChange={onChangeHandler} />
+      <input
+        type="text"
+        value={input}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDownHandler}
+      />
       {console.log("??", input)}
     </>
   );
